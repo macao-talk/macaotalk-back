fcf116d683c35b4d6c723daaf2e051a632185bec
syk25 <syk2245@gmail.com>
2023-12-13 20:06:29 +0900

    init: First Commit

---
+++ src/main/java/com/syk25/macaotalk/MacaotalkApplication.java
@@ -0,0 +1,13 @@
+package com.syk25.macaotalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacaotalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacaotalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/syk25/macaotalk/MacaotalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syk25.macaotalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacaotalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
